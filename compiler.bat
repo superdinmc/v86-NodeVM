699ef1b9903ba8781fa4e21fe128ab26aa6d754f
5UP3R_D1N <42496449+5UP3R-D1N@users.noreply.github.com>
2022-12-08 07:54:05 +0700

    A lot of lots of updates

--- compiler.bat
+++ compiler.bat
@@ -1,8 +1,11 @@
 @echo off
 cls
 color 02
-echo Bundling files...
-java -jar compiler.jar --compilation_level SIMPLE --js libv86.js nodejs.js --js_output_file NodeVM.js --warning_level QUIET
-echo Starting...
-color
-node NodeVM.js
+REM echo Bundling files...
+REM java -jar compiler.jar --compilation_level BUNDLE --js libv86.js nodejs.js --js_output_file NodeVM.js --warning_level QUIET
+echo Minifying package...
+java -jar compiler.jar --compilation_level SIMPLE --js nodejs.js --js_output_file NodeVM.js --warning_level QUIET
+REM echo Starting...
+REM color
+REM node NodeVM.js
+echo Done!
